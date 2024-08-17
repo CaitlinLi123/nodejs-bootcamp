d8debeed033cd1b0be11ce2fbc2922c04fa13a53
CaitlinLi123 <104264689+CaitlinLi123@users.noreply.github.com>
2024-08-17 21:01:12 +0100

    feat: send the data to new route

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-import express from "express";
+const express = require("express");
+const fs = require("fs");
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
 const app = express();
-app.get("/", (req, res) => {
-  res
-    .status(200)
-    .json({ message: "hello from the server side!", app: "Natours" });
-});
-
-app.post("/", (req, res) => {
-  res.send("You can post to this endpoint.");
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: { tours },
+  });
 });
 
 const port = 3000;
