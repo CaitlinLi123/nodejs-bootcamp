a21e4ad142801c063a278be48260503ca30fdb19
CaitlinLi123 <104264689+CaitlinLi123@users.noreply.github.com>
2024-08-17 21:18:02 +0100

    feat: develop the post method for the route

--- app.js
+++ app.js
@@ -6,11 +6,39 @@ const tours = JSON.parse(
 );
 
 const app = express();
+
+//middleware
+app.use(express.json());
+
 app.get("/api/v1/tours", (req, res) => {
   res.status(200).json({
     status: "success",
     data: { tours },
   });
+});
+
+//params example:/api/v1/tours/:id/:x/:y
+//optional parameters (make y optional): /api/v1/:id/:y?
+app.get("/api/v1/tours/:id", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    //   data: { tours },
+  });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  //merge and create a new object: Object.assign
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  //inside the eventloop, cannot block it, thus we need to use asynchronous version of write file
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({ status: "success", data: { tour: newTour } });
+    }
+  );
 });
 
 const port = 3000;
