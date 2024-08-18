2cb9954bfa33d35ff79c50065fe1afbd14b03dd3
CaitlinLi123 <104264689+CaitlinLi123@users.noreply.github.com>
2024-08-18 15:32:57 +0100

    feat: create a mongodb model

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 //this file is not related to express codes
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
@@ -9,6 +10,23 @@ const app = require("./app");
 
 //this is set by nodejs
 // console.log(process.env);
+
+const DB = process.env.DATABASE_LOCAL;
+mongoose.connect(DB).then(() => {
+  console.log("MongoDB connection successful!");
+});
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A tour must have a name!"],
+    unique: true,
+  },
+  rating: { type: Number, default: 4.5 },
+  price: { type: Number, required: [true, "A tour must have a price!"] },
+});
+
+const Tour = mongoose.model("Tour", tourSchema);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
