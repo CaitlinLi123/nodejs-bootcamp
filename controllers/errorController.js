d7ac67fe6182c3cc19314d0a27576b4b945483aa
CaitlinLi123 <104264689+CaitlinLi123@users.noreply.github.com>
2024-08-21 11:01:51 +0100

    feat: add error handling functions to MongoDB errors under production enviornment

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,5 @@
+const AppError = require("../utils/appError");
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -5,6 +7,26 @@ const sendErrorDev = (err, res) => {
     message: err.message,
     stack: err.stack,
   });
+};
+
+const handleCastErrorDB = (err) => {
+  //path: which part is wrong
+  //value:the value of the wrong part
+  const message = `Invalid ${err.path}: ${err.value}`;
+  //bad request
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0];
+  const message = `Duplicate Field Value: ${value}. Please use another value!   `;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid Input Data. ${errors.join(". ")}`;
+  return new AppError(message, 400);
 };
 
 const sendErrorProd = (err, res) => {
@@ -34,6 +56,12 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === "development") {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === "production") {
-    sendErrorProd(err, res);
+    //MongoDB Error
+    let error = { ...err };
+    if (error.name === "CastError") error = handleCastErrorDB(error);
+    if (error.name === "11000") error = handleDuplicateFieldsDB(error);
+    if (error.name === " ValidationError")
+      error = handleValidationErrorDB(error);
+    sendErrorProd(error, res);
   }
 };
