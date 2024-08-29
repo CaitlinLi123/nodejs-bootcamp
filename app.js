c6800c8bb5a24f924924d4f3c81ee468dbbf402e
CaitlinLi123 <104264689+CaitlinLi123@users.noreply.github.com>
2024-08-29 18:34:04 +0100

    feat: set rate limit

--- app.js
+++ app.js
@@ -4,15 +4,22 @@ const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
+const rateLimit = require("express-rate-limit");
 
 const app = express();
 
-// 1) MIDDLEWARE
+// 1) GLOBAL MIDDLEWARE
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour.",
+});
+app.use("/api", limiter);
 app.use(express.json()); //bodyParser
 app.use(express.static(`${__dirname}/public`)); //serve static file
 
