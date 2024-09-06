d6ae1ba276b5fb5b6caa68efd53492700d24439d
CaitlinLi123 <104264689+CaitlinLi123@users.noreply.github.com>
2024-09-06 14:49:06 +0100

    feat: build the overview templace

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 const reviewRouter = require("./routes/reviewRoutes");
+const viewRouter = require("./routes/viewRoutes");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 const rateLimit = require("express-rate-limit");
@@ -70,29 +71,12 @@ app.use((req, res, next) => {
 });
 
 // 2) ROUTES
-app.get("/", (req, res) => {
-  res.status(200).render("base", {
-    tour: "The Forest Hiker",
-    user: "Jonas",
-  });
-});
-
-app.get("/overview", (req, res) => {
-  res.status(200).render("overview", {
-    title: "All Tours",
-  });
-});
-
-app.get("/tour", (req, res) => {
-  res.status(200).render("tour", {
-    title: "The Forest Hiker Tour",
-  });
-});
 
 //Mount the routes
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/reviews", reviewRouter);
+app.use("/", viewRouter);
 
 //for all the http methods:
 app.all("*", (req, res, next) => {
