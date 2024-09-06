962e5504cf3b4a75c3d6fca0346b37b26c01abdc
CaitlinLi123 <104264689+CaitlinLi123@users.noreply.github.com>
2024-09-06 09:03:55 +0100

    feat: extend base template with blocks

--- app.js
+++ app.js
@@ -70,10 +70,22 @@ app.use((req, res, next) => {
 });
 
 // 2) ROUTES
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
   res.status(200).render("base", {
     tour: "The Forest Hiker",
     user: "Jonas",
+  });
+});
+
+app.get("/overview", (req, res) => {
+  res.status(200).render("overview", {
+    title: "All Tours",
+  });
+});
+
+app.get("/tour", (req, res) => {
+  res.status(200).render("tour", {
+    title: "The Forest Hiker Tour",
   });
 });
 
