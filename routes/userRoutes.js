8ed36c307b6e215532519ca67b73d1e2f040fdc7
CaitlinLi123 <104264689+CaitlinLi123@users.noreply.github.com>
2024-09-08 12:27:04 +0100

    feat: add user upload photo feature

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,10 @@
 const userController = require("../controllers/userController.js");
 const authController = require("../controllers/authController.js");
 const express = require("express");
+const multer = require("multer");
+
+//the path to save all the images
+const upload = multer({ dest: "public/img/users" });
 
 const router = express.Router();
 
@@ -15,7 +19,13 @@ router.use(authController.protect);
 
 router.patch("/updateMyPassword", authController.updatePassword);
 router.get("/me", userController.getMe, userController.getUser);
-router.patch("/updateMe", userController.updateMe);
+//upload a single file, specify the name of the field we want to hold, this middleware will take the file and copy it to the destination
+router.patch(
+  "/updateMe",
+  userController.uploadUserPhoto,
+  userController.resizeUploadedPhoto,
+  userController.updateMe
+);
 router.delete("/deleteMe", userController.deleteMe);
 
 router.use(authController.restrictTo("admin"));
