acf979f8127c15cfaba120c61cf05ac30b6f2a14
Yu Li <liyu3519@gmail.com>
2024-09-10 17:05:10 +0100

    feat: finish payments with stripe webhooks

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 const reviewRouter = require("./routes/reviewRoutes");
 const bookingRouter = require("./routes/bookingRoutes");
+const bookingController = require("./controllers/bookingController");
 const viewRouter = require("./routes/viewRoutes");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -70,6 +71,13 @@ const limiter = rateLimit({
   message: "Too many requests from this IP, please try again in an hour.",
 });
 app.use("/api", limiter);
+
+//stripe needs a raw string body, not a json format. So this is put above the body parser middleware
+app.post(
+  "/webhook-checkout",
+  express.raw({ type: "application/json" }),
+  bookingController.webhookCheckout
+);
 
 // Body parser, reading data from body into req.body
 //set limit to the data size sent to server
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get(
   "/",
-  bookingController.createBookingCheckout,
+  bookingController.webhookCheckout,
   authController.isLoggedIn,
   viewController.getOverview
 );
